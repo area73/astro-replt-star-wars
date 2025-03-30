1d9c7a9ff35ee410d9876fe6f3a5098cb767a9c2
rerades <rerades@siete3.com>
2025-03-30 13:16:45 +0200

    adding initial infrastructure

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 export default {
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5000,
     strictPort: true,
     hmr: {
       clientPort: 443,
-      protocol: 'wss'
+      protocol: "wss",
     },
     watch: {
-      usePolling: true
+      usePolling: true,
     },
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*",
     },
-    allowedHosts: ['fa96b03d-e089-458e-a060-6a1e3398846f-00-2l4u6m5zd8isa.janeway.replit.dev', 'replit.dev', '.replit.dev', '*.replit.dev']
-  }
-}
+    allowedHosts: ["*.replit.dev"],
+  },
+};
